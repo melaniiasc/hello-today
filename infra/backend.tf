eea4e670e8ce0cef3f0286e40d186dab3677199e
stlvm <melaniia.s2001@gmail.com>
2026-07-24 16:03:05 +0300

    Format terraform files

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "hello-today-state-bucket"
-    key = "terraform.tfstate"
+    key    = "terraform.tfstate"
     region = "us-east-1"
   }
 }
--- infra/main.tf
+++ infra/main.tf
@@ -56,12 +56,12 @@ resource "aws_iam_role_policy" "lambda_policy" {
 }
 
 resource "aws_lambda_function" "scheduled_lambda" {
-  function_name = "scheduled-s3-writer"
-  runtime = "python3.12"
-  handler = "app.handler.lambda_handler"
-  filename = "lambda.zip"
+  function_name    = "scheduled-s3-writer"
+  runtime          = "python3.12"
+  handler          = "app.handler.lambda_handler"
+  filename         = "lambda.zip"
   source_code_hash = filebase64sha256("lambda.zip")
-  role = aws_iam_role.lambda_role.arn
+  role             = aws_iam_role.lambda_role.arn
 }
 
 resource "aws_iam_role" "scheduler_role" {
@@ -85,16 +85,16 @@ resource "aws_iam_role_policy" "scheduler_policy" {
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Effect = "Allow"
-      Action = "lambda:InvokeFunction"
+      Effect   = "Allow"
+      Action   = "lambda:InvokeFunction"
       Resource = aws_lambda_function.scheduled_lambda.arn
     }]
   })
 }
 
 resource "aws_scheduler_schedule" "schedule" {
-  name = "hello-today-schedule"
-  group_name = "default"
+  name                = "hello-today-schedule"
+  group_name          = "default"
   schedule_expression = var.schedule_expression
 
   flexible_time_window {
@@ -102,15 +102,15 @@ resource "aws_scheduler_schedule" "schedule" {
   }
 
   target {
-    arn = aws_lambda_function.scheduled_lambda.arn
+    arn      = aws_lambda_function.scheduled_lambda.arn
     role_arn = aws_iam_role.scheduler_role.arn
   }
 }
 
 resource "aws_lambda_permission" "allow_scheduler" {
-  statement_id = "AllowScheduler"
-  action = "lambda:InvokeFunction"
+  statement_id  = "AllowScheduler"
+  action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.scheduled_lambda.function_name
-  principal = "scheduler.amazonaws.com"
-  source_arn = aws_scheduler_schedule.schedule.arn
+  principal     = "scheduler.amazonaws.com"
+  source_arn    = aws_scheduler_schedule.schedule.arn
 }
