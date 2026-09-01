d7805fc92ce736b6a493c6f563c470560fd65338
stlvm <melaniia.s2001@gmail.com>
2026-09-01 12:35:06 +0300

    bcd

--- infra/modules/observability/variables.tf
+++ infra/modules/observability/variables.tf
@@ -5,7 +5,8 @@ variable "region" {
 variable "alert_email" {
   description = "Email for alert"
   type        = string
-}
+  default = "melaniia.s2001@gmail.com"
+}c
 
 variable "environment" {
   description = "Deployment environment"
